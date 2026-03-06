7b1db3cddb9780bfce0b596ea335b19394ccf21c
pppp-hhhh <2234668853@qq.com>
2026-03-06 18:56:48 +0800

    创建了Nuxt项目
    将原本的静态web转变为Nuxt项目，方便后续增减、修改页面

---
+++ web_demo/backend/web/app/layouts/default.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="app">
+    <main class="page">
+      <slot />
+    </main>
+    
+    <footer class="footer">
+      <p>made by 无畏契约赋能组</p>
+    </footer>
+  </div>
+</template>
+
+<style scoped>
+.app {
+  min-height: 100vh;
+  padding: 24px;
+  //background: #0f172a;
+}
+
+.page {
+  max-width: 1400px;
+  margin: 0 auto;
+}
+
+.footer {
+  text-align: center;
+  padding: 24px;
+  color: #86909c;
+  font-size: 14px;
+  margin-top: 24px;
+}
+</style>
